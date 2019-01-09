ae1d7d3a167d86c7d2aa38d873ee2e85ed91471e
Noor <aftab22n@mtholyoke.edu>
2019-01-09 14:47:36 -0500

    Added code to handle computer's turn for game

--- app/src/main/java/com/example/demouser/dicey/MainActivity.java
+++ app/src/main/java/com/example/demouser/dicey/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.demouser.dicey;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.demouser.dicey.R;
+
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private int userTurnScore;
     private int computerOverallScore;
     private int computerTurnScore;
+    private int diceValue;
 
     private ImageView dice;
     private Button rollButton;
@@ -31,113 +38,208 @@ public class MainActivity extends AppCompatActivity {
     private TextView yourScore;
     private TextView comScore;
 
+    private boolean computerTurn = true;
+
     private Random random = new Random();
+
+    private Handler handler = new Handler();
+    private Runnable r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        yourScore = findViewById(R.id.yourScore);
+        //initialize UI components in main code
+        yourScore = findViewById(R.id.yourScore);   //textviews
         comScore = findViewById(R.id.comScore);
         extraScore = findViewById(R.id.extra);
 
-        dice = findViewById(R.id.oneDice);
+        dice = findViewById(R.id.oneDice);  //images
 
-        rollButton = findViewById(R.id.roll);
+        rollButton = findViewById(R.id.roll);   //buttons
         holdButton = findViewById(R.id.hold);
-        resetButton = findViewById(R.id.insert);
+        resetButton = findViewById(R.id.reset);
 
-        //userTurnScore = 0;
+        extraScore.setText("User plays first! Press roll to start :)");
 
+        //initialize action listeners & define functionality for buttons
         rollButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
+                rollButton.setEnabled(true);
+                holdButton.setEnabled(true);
 
                 doARoll();
+                //if 1
+                if (diceValue==1) {
+                    //reset turn score and reset text label
+                    userTurnScore = 0;
+                    extraScore.setText("Your Turn Score = " + userTurnScore);
+                    userOverallScore += userTurnScore;
+                    yourScore.setText("Your Score = " + userOverallScore);
 
+                    //computer's turn
+                    computerTurn();
+
+                }
+                else{
+                    userTurnScore += diceValue; //update the turn score
+                    extraScore.setText("Your Turn Score = " + userTurnScore);   //update the label
+                }
             }
-
         });
 
         resetButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
+                //reset all scores to 0
                 userOverallScore = 0;
                 userTurnScore = 0;
                 computerOverallScore = 0;
                 computerTurnScore= 0;
+                //reset text labels to reflect reset scores
+                yourScore.setText("Your score = " + userOverallScore);
+                comScore.setText("Computer score = " + computerOverallScore);
+                extraScore.setText("Play a turn!");
 
-                yourScore.setText("Your score = " + 0);
-                comScore.setText("Computer score = " + 0);
-                extraScore.setText("Play a turn");
-
+                rollButton.setEnabled(true);
+                holdButton.setEnabled(true);
             }
         });
 
         holdButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
+                //update user's overall score by adding turn score & update label to reflect that
+                userOverallScore += userTurnScore;
 
-                userOverallScore = userOverallScore + userTurnScore;
-                yourScore.setText("Your Score = " + userOverallScore);
-                userTurnScore = 0;
-                extraScore.setText("Your Turn Score = " + userTurnScore);
+                if (userOverallScore >= 100) {
+                    extraScore.setText("Computer wins! Press restart to play again.");
+                    rollButton.setEnabled(false);
+                    holdButton.setSaveEnabled(false);
+                }
+                else {
+                    yourScore.setText("Your Score = " + userOverallScore);
+                    //reset turn score and reset text label
+                    userTurnScore = 0;
+                    extraScore.setText("Turn Score = " + userTurnScore);
+                    //now it's computer's turn
+                    computerTurn();
+                }
 
             }
-
         });
 
     }
 
+    /**
+     * helper method when it's the computer's turn
+     * disables roll and hold buttons & while loop to loop over computer's turns
+     */
+    private void computerTurn() {
 
 
+
+        final Handler handler = new Handler();
+        r = new Runnable() {
+            @Override
+            public void run() {
+
+                rollButton.setEnabled(false);
+                holdButton.setEnabled(false);
+                doARoll();
+                computerTurnScore += diceValue;
+
+                if (computerOverallScore < 100) {
+
+                    if (computerTurnScore >= 20) {
+                        //hold there
+                        extraScore.setText("Turn Score = " + computerTurnScore);
+                        computerOverallScore += computerTurnScore;
+                        comScore.setText("Computer score = " + computerOverallScore);
+                        //reset turn score and reset text label
+                        computerTurnScore = 0;
+
+                    }
+                    else if (diceValue == 1) {
+                        computerTurnScore = 0;
+                        extraScore.setText("Turn Score = " + computerTurnScore);
+                        computerOverallScore += computerTurnScore;
+                        comScore.setText("Computer score = " + computerOverallScore);
+                        // computerTurn = false;
+                    }
+
+                    else if (diceValue>1){
+                        //pause to see image change
+                        extraScore.setText("Turn Score = " + computerTurnScore);
+                        handler.postDelayed(this, 500);
+                    }
+
+
+                }
+                if (computerOverallScore>= 100) {
+                    extraScore.setText("Computer wins! Press restart to play again.");
+                    rollButton.setEnabled(false);
+                    holdButton.setEnabled(false);
+
+                }
+
+            }
+        };
+        handler.postDelayed(r, 500);
+
+        //re-enable buttons
+        rollButton.setEnabled(true);
+        holdButton.setEnabled(true);
+
+
+    }
+
+    /**
+     * Method invoked when the roll button is pressed.
+     * Contains rules of the game, when dice rolls a 1, turn gets 0, else turn gets # rolled.
+     */
     private void doARoll() {
-
-        int diceValue = random.nextInt(6) + 1;
-
-        //could've put dice in an array
+        //dice rolls a random #
+        diceValue = random.nextInt(6-1) + 1;
+        // Log.i("test7", "diceValue: "+diceValue);
 
         switch(diceValue) {
             case 1:
                 dice.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.dice1, null));
-                userTurnScore = 0;
-                extraScore.setText("Your Turn Score = " + 0);
+                diceValue = 1;  //update score
+                // extraScore.setText("Your Turn Score = " + 0);   //update turn score label
                 break;
 
             case 2:
                 dice.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.dice2, null));
-                userTurnScore += diceValue;
-                extraScore.setText("Your Turn Score = " + userTurnScore);
+                //userTurnScore += diceValue;
+                // extraScore.setText("Your Turn Score = " + userTurnScore);
                 break;
 
             case 3:
                 dice.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.dice3, null));
-                userTurnScore += diceValue;
-                extraScore.setText("Your Turn Score = " + userTurnScore);
+                //userTurnScore += diceValue;
+                // extraScore.setText("Your Turn Score = " + userTurnScore);
                 break;
 
             case 4:
                 dice.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.dice4, null));
-                userTurnScore += diceValue;
-                extraScore.setText("Your Turn Score = " + userTurnScore);
+                // userTurnScore += diceValue;
+                //extraScore.setText("Your Turn Score = " + userTurnScore);
                 break;
 
             case 5:
                 dice.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.dice5, null));
-                userTurnScore += diceValue;
-                extraScore.setText("Your Turn Score = " + userTurnScore);
+                // userTurnScore += diceValue;
+                //extraScore.setText("Your Turn Score = " + userTurnScore);
                 break;
             case 6:
                 dice.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.dice6, null));
-                userTurnScore += diceValue;
-                extraScore.setText("Your Turn Score = " + userTurnScore);
+                // userTurnScore += diceValue;
+                // extraScore.setText("Your Turn Score = " + userTurnScore);
                 break;
-
-
         }
 
     }
